bb5537cf341d6e347c8c24787f80efe6febb7090
ATASIE_GLORIA <atasiegloria@gmail.com>
2026-05-07 10:12:13 +0100

    Update accountRoutes.js

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const auth = require("../middleware/authMiddleware");
-const { createAccount } = require("../controllers/accountController");
+const { createAccount, getAccount, getTransactions } = require("../controllers/accountController");
 
 router.post("/create", auth, createAccount);
+router.get("/me", auth, getAccount);           // ← get balance + account number
+router.get("/transactions", auth, getTransactions); // ← get transaction history
 
 module.exports = router;
